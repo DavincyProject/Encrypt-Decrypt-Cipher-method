db9974e2d170f8e1ebf26d46b7b0aa5a63b73efa
Billy <76769657+DavincyProject@users.noreply.github.com>
2023-03-19 14:34:24 +0700

    fix shift from max 6 shift now can be up to 25

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function shiftCharCode(char, shift) {
     let charCode = char.charCodeAt(0);
     if (charCode >= 65 && charCode <= 90) {
         // uppercase letters
-        charCode = ((charCode - 65 + shift) % 26) + 65;
+        charCode = ((charCode - 65 + (shift % 26) + 26) % 26) + 65;
     } else if (charCode >= 97 && charCode <= 122) {
         // lowercase letters
-        charCode = ((charCode - 97 + shift) % 26) + 97;
+        charCode = ((charCode - 97 + (shift % 26) + 26) % 26) + 97;
+    } else {
+        return char;
     }
     return String.fromCharCode(charCode);
 }
